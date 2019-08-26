a5f0f7d3ee34c908f5f3534fc2b68e11408036f0
Martin Egli <martin@eglinet.ch>
2019-08-26 07:18:39 +0200

    Update Build AppVeyor

--- Source/Anori.Cake.Recipe/Content/version.cake
+++ Source/Anori.Cake.Recipe/Content/version.cake
@@ -1,6 +1,6 @@
 
         public class BuildMetaData
         {
-            public static string Date { get; } = "2019-06-19 12:19:15";
-            public static string Version { get; } = "0.1.24";
+            public static string Date { get; } = "2019-08-25 13:05:24";
+            public static string Version { get; } = "1.1.0";
         }
---
+++ buildAppVeyor.cmd
@@ -0,0 +1,13 @@
+@echo off
+
+set scriptFileName=.\Tools\Resources\build.ps1 -Target AppVeyor
+set scriptFolderPath=%~dp0
+
+rem set command="Start-Process powershell \"-ExecutionPolicy Bypass -NoProfile -NoExit -Command `\"cd \`\"%scriptFolderPath%`\"; & \`\"%scriptFileName%\`\"`\"\" -Verb RunAs"
+set command="%scriptFileName%"
+
+
+cd %scriptFolderPath%
+echo %command%
+powershell -Command %command%
+rem pause
